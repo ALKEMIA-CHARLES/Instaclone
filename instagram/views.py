7ea5ccb50a0036147684e07a67eb20e64a87ef83
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-01-21 15:55:14 +0300

    added database users

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-
+from instagram.models import DBUSER, Comments, loggedinUser
 # Create your views here.
 
 def index(request):
-    return render(request, "main/index.html")
+    dbusers = DBUSER.show_db_users()
+    return render(request, "main/index.html", context={"dbusers":dbusers})
