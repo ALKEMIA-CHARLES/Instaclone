fc7dbcccb48dbc313da15b12a17a5ade19675efa
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-02-10 10:27:32 +0300

    added static

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -136,6 +136,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'media')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
